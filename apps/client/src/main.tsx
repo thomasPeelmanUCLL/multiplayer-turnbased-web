f88f100357b57e760c108cb62c2706bca754203f
pants <145909640+thomasPeelmanUCLL@users.noreply.github.com>
2026-07-02 02:15:38 +0200

    fix: wrap app in RoomProvider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.js';
+import { RoomProvider } from './context/RoomContext.js';
 import { App } from './App.js';
 import './index.css';
 
@@ -9,7 +10,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <RoomProvider>
+          <App />
+        </RoomProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
